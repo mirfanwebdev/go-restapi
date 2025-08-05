43e276797a4fe62f49a58d4710524039493dbbed
m.irfan <m.irfanwebdev@gmail.com>
2025-08-05 23:07:55 +0700

    add tokenAuthMiddleware

---
+++ auth.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+)
+
+func TokenAuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var clientId = r.URL.Query().Get("clientId")
+		clientProfile, ok := database[clientId]
+		if !ok || clientId == "" {
+			http.Error(w, "Forbidden", http.StatusForbidden)
+			return
+		}
+	}
+}
+
+func isValidToken(clientProfile ClientProfile, token string) bool {
+	if strings.HasPrefix(token, "Bearer ") {
+		return strings.TrimPrefix(token, "Bearer ") == clientProfile.Token
+	}
+	return false
+}
